9f1a20f09674677972e72f0ea5d7f8864018dc9b
benkimz <benkim3619@gmail.com>
2024-02-24 18:55:33 +0300

    Initial UI Template

--- src/MilesAhead.Components/Models/BaseModels/BaseWireFrame.cs
+++ src/MilesAhead.Components/Models/BaseModels/BaseWireFrame.cs
@@ -8,7 +8,7 @@ public class BaseWireFrame : IBaseWireFrame
     [MaxLength(255)]
     public string Name { get; set; } = "Dynamic Component";
     [MaxLength(64)]
-    public required string Segment { get; set; } // target component or html element
+    public string Segment { get; set; } = "div"; // target component or html element
     public List<WireFrameAttribute>? Attributes { get; set; } // segment attributes
     public RenderData? Data { get; set; } // data to be rendered
 }
---
+++ src/MilesAhead.Web/RenderFactory/HomePrimeRoot.cs
@@ -0,0 +1,13 @@
+﻿using MilesAhead.Components;
+
+namespace MilesAhead.Web;
+
+public class HomePrimeRoot
+{
+    protected PrimeRoot Root { get; set; } = new PrimeRoot();
+
+    protected PrimeRoot Build()
+    {
+        return Root;
+    }
+}
